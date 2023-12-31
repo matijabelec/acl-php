0cfea5533876bd8a06a65a265be914ed4e5ad2de
Matija Belec <matija@belec.dev>
2023-12-31 12:44:24 +0100

    Cleanup Code

--- src/Acl.php
+++ src/Acl.php
@@ -1,10 +1,12 @@
 <?php
 
+declare(strict_types=1);
+
 /**
  * Acl
  *
  * @package    acl-php
- * @author     Matija Belec <matijabelec1@gmail.com>
+ * @author     Matija Belec <matija@belec.dev>
  * @copyright  2017 Matija Belec
  * @license    MIT
  * @link       https://github.com/matijabelec/acl-php
@@ -12,145 +14,107 @@
 
 namespace MatijaBelec\Acl;
 
-/**
- * Acl
- */
-class Acl {
+final class Acl
+{
+    /**
+     * @var int
+     */
+    public const DENIED = 0;
 
-  /**
-   * Denied action.
-   * @var integer
-   */
-  const DENIED = 0;
+    /**
+     * @var int
+     */
+    public const ALLOWED = 1;
 
-  /**
-   * Allowed action.
-   * @var integer
-   */
-  const ALLOWED = 1;
+    protected array $roles;
 
-  /**
-   * Roles hierarchy.
-   * @var array
-   */
-  protected $roles;
+    protected array $actions;
 
-  /**
-   * Actions per roles.
-   * @var array
-   */
-  protected $actions;
+    public function allow(string $role, string|array $actions): void
+    {
 
-  /**
-   * Add allowed actions to selected role.
-   * @param  string $role           role for which actions are added
-   * @param  string|array $actions  actions that are added for role
-   * @return void
-   */
-  public function allow($role, $actions) {
+        // create array of actions (in any case: if $actions is string or array)
+        $acts = [];
+        if (is_string($actions)) {
+            $acts[] = $actions;
+        } else if (is_array($actions)) {
+            $acts = $actions;
+        }
 
-    // create array of actions (in any case: if $actions is string or array)
-    $acts = [];
-    if(is_string($actions)) {
-      $acts[] = $actions;
-    } else if(is_array($actions)) {
-      $acts = $actions;
+        // check if actions for selected role are already set or create new key
+        if (isset($this->actions[$role])) {
+            foreach ($acts as $act) {
+                $this->actions[$role][$act] = Acl::ALLOWED;
+            }
+        } else {
+            foreach ($acts as $act) {
+                $this->actions[$role] = [$act => Acl::ALLOWED];
+            }
+        }
     }
 
-    // check if actions for selected role are already set or create new key
-    if(isset($this->actions[$role])) {
-      foreach($acts as $act) {
-        $this->actions[$role][$act] = Acl::ALLOWED;
-      }
-    } else {
-      foreach($acts as $act) {
-        $this->actions[$role] = [$act => Acl::ALLOWED];
-      }
-    }
-  }
+    public function deny(string $role, string|array $actions): void
+    {
 
-  /**
-   * Add denied actions to selected role.
-   * @param  string $role           role for which actions are added
-   * @param  string|array $actions  actions that are added for role
-   * @return void
-   */
-  public function deny($role, $actions) {
+        // create array of actions (in any case: if $actions is string or array)
+        $acts = [];
+        if (is_string($actions)) {
+            $acts[] = $actions;
+        } else if (is_array($actions)) {
+            $acts = $actions;
+        }
 
-    // create array of actions (in any case: if $actions is string or array)
-    $acts = [];
-    if(is_string($actions)) {
-      $acts[] = $actions;
-    } else if(is_array($actions)) {
-      $acts = $actions;
+        // check if actions for selected role are already set or create new key
+        if (isset($this->actions[$role])) {
+            foreach ($acts as $act) {
+                $this->actions[$role][$act] = Acl::DENIED;
+            }
+        } else {
+            foreach ($acts as $act) {
+                $this->actions[$role] = [$act => Acl::DENIED];
+            }
+        }
     }
 
-    // check if actions for selected role are already set or create new key
-    if(isset($this->actions[$role])) {
-      foreach($acts as $act) {
-        $this->actions[$role][$act] = Acl::DENIED;
-      }
-    } else {
-      foreach($acts as $act) {
-        $this->actions[$role] = [$act => Acl::DENIED];
-      }
-    }
-  }
+    public function isAllowed(string $role, string $action): bool
+    {
 
-  /**
-   * Check if action is allowed for selected role.
-   * @param  string  $role   name of role
-   * @param  string  $action name of action to be checked with
-   * @return boolean         returns true if action is allowed, false otherwise
-   */
-  public function isAllowed($role, $action) {
+        // check direct actions set for role
+        if (isset($this->actions[$role]) && isset($this->actions[$role][$action])) {
+            return ($this->actions[$role][$action] === Acl::ALLOWED);
+        }
 
-    // check direct actions set for role
-    if(isset($this->actions[$role]) && isset($this->actions[$role][$action])) {
-      return ($this->actions[$role][$action] === Acl::ALLOWED);
+        // check parent actions recursively
+        if (isset($this->roles[$role])) {
+            $parent = $this->roles[$role];
+            return $this->isAllowed($parent, $action);
+        }
+
+        // return false (action denied is default) if action not strictly allowed
+        return false;
     }
 
-    // check parent actions recursively
-    if(isset($this->roles[$role])) {
-      $parent = $this->roles[$role];
-      return isAllowed($parent, $action);
+    public function isDenied(string $role, string $action): bool
+    {
+        return !($this->isAllowed($role, $action));
     }
 
-    // return false (action denied is default) if action not strictly allowed
-    return false;
-  }
+    public function addRole(string $role, ?string $parent = null): bool
+    {
 
-  /**
-   * Check if action is denied for selected role.
-   * @param  string  $role   name of role
-   * @param  string  $action name of action to be checked with
-   * @return boolean         returns true if action is denied, false otherwise
-   */
-  public function isDenied($role, $action) {
-    return !($this->isAllowed($role, $action));
-  }
+        // check if parent is not set then set new role directly (value is null)
+        if (is_null($parent)) {
+            $this->roles[$role] = null;
+            return true;
+        }
 
-  /**
-   * Add role new role with optionally set parent.
-   * @param string $role    new role name
-   * @param string $parent  parent name of role or null if no parent (optional)
-   * @return boolean        returns success of adding of role
-   */
-  public function addRole($role, $parent=null) {
+        // check if valid parent is set before setting new role with parent
+        if (is_string($parent) && isset($this->roles[$parent])) {
+            $this->roles[$role] = $parent;
+            return true;
+        }
 
-    // check if parent is not set then set new role directly (value is null)
-    if(is_null($parent)) {
-      $this->roles[$role] = null;
-      return true;
+        // return false if any error occurred
+        return false;
     }
-
-    // check if valid parent is set before setting new role with parent
-    if(is_string($parent) && isset($this->roles[$parent])) {
-      $this->roles[$role] = $parent;
-      return true;
-    }
-
-    // return false if any error occured
-    return false;
-  }
 }
--- tests/AclTest.php
+++ tests/AclTest.php
@@ -1,45 +1,49 @@
 <?php
 
+declare(strict_types=1);
+
 use MatijaBelec\Acl\Acl;
+use PHPUnit\Framework\TestCase;
 
-class AclTest extends PHPUnit_Framework_TestCase {
-  public function testRolesWithActions()
-  {
-    $acl = new Acl();
+final class AclTest extends TestCase
+{
+    public function testRolesWithActions(): void
+    {
+        $acl = new Acl();
 
-    // create role hierarchy
-    $acl->addRole('guest');
-    $acl->addRole('user', 'guest');
-    $acl->addRole('administrator', 'user');
+        // create role hierarchy
+        $acl->addRole('guest');
+        $acl->addRole('user', 'guest');
+        $acl->addRole('administrator', 'user');
 
-    // create actions tree
-    $acl->allow('guest', 'canUpdate');
-    $acl->allow('guest', 'canDelete');
+        // create actions tree
+        $acl->allow('guest', 'canUpdate');
+        $acl->allow('guest', 'canDelete');
 
-    $acl->allow('user', 'canCreate');
-    $acl->deny('user', 'canDelete');
+        $acl->allow('user', 'canCreate');
+        $acl->deny('user', 'canDelete');
 
-    $acl->allow('administrator', ['canRead', 'canUpdate']);
-    $acl->allow('administrator', 'canDelete');
+        $acl->allow('administrator', ['canRead', 'canUpdate']);
+        $acl->allow('administrator', 'canDelete');
 
-    $this->assertNotTrue($acl->isAllowed('administrator', 'canRead'), 'Administrator canRead should be denied');
-    $this->assertTrue($acl->isAllowed('administrator', 'canDelete'), 'Administrator canDelete should be allowed');
-    $this->assertTrue($acl->isAllowed('administrator', 'canUpdate'), 'Administrator canUpdate should be allowed');
-    $this->assertNotTrue($acl->isAllowed('user', 'canDelete'), 'User canDelete should be denied');
-    $this->assertTrue($acl->isAllowed('user', 'canCreate'), 'User canCreate should be allowed');
-    $this->assertNotTrue($acl->isAllowed('user', 'canRead'), 'User canRead should be denied');
-    $this->assertNotTrue($acl->isAllowed('guest', 'canRead'), 'Guest canRead should be denied');
-    $this->assertTrue($acl->isAllowed('guest', 'canDelete'), 'Guest canDelete should be allowed');
-    $this->assertTrue($acl->isAllowed('guest', 'canUpdate'), 'Guest canUpdate should be allowed');
+        $this->assertNotTrue($acl->isAllowed('administrator', 'canRead'), 'Administrator canRead should be denied');
+        $this->assertTrue($acl->isAllowed('administrator', 'canDelete'), 'Administrator canDelete should be allowed');
+        $this->assertTrue($acl->isAllowed('administrator', 'canUpdate'), 'Administrator canUpdate should be allowed');
+        $this->assertNotTrue($acl->isAllowed('user', 'canDelete'), 'User canDelete should be denied');
+        $this->assertTrue($acl->isAllowed('user', 'canCreate'), 'User canCreate should be allowed');
+        $this->assertNotTrue($acl->isAllowed('user', 'canRead'), 'User canRead should be denied');
+        $this->assertNotTrue($acl->isAllowed('guest', 'canRead'), 'Guest canRead should be denied');
+        $this->assertTrue($acl->isAllowed('guest', 'canDelete'), 'Guest canDelete should be allowed');
+        $this->assertTrue($acl->isAllowed('guest', 'canUpdate'), 'Guest canUpdate should be allowed');
 
-    $this->assertTrue($acl->isDenied('administrator', 'canRead'), 'Administrator canRead should be denied');
-    $this->assertNotTrue($acl->isDenied('administrator', 'canDelete'), 'Administrator canDelete should be allowed');
-    $this->assertNotTrue($acl->isDenied('administrator', 'canUpdate'), 'Administrator canUpdate should be allowed');
-    $this->assertTrue($acl->isDenied('user', 'canDelete'), 'User canDelete should be denied');
-    $this->assertNotTrue($acl->isDenied('user', 'canCreate'), 'User canCreate should be allowed');
-    $this->assertTrue($acl->isDenied('user', 'canRead'), 'User canRead should be denied');
-    $this->assertTrue($acl->isDenied('guest', 'canRead'), 'Guest canRead should be denied');
-    $this->assertNotTrue($acl->isDenied('guest', 'canDelete'), 'Guest canDelete should be allowed');
-    $this->assertNotTrue($acl->isDenied('guest', 'canUpdate'), 'Guest canUpdate should be allowed');
-  }
+        $this->assertTrue($acl->isDenied('administrator', 'canRead'), 'Administrator canRead should be denied');
+        $this->assertNotTrue($acl->isDenied('administrator', 'canDelete'), 'Administrator canDelete should be allowed');
+        $this->assertNotTrue($acl->isDenied('administrator', 'canUpdate'), 'Administrator canUpdate should be allowed');
+        $this->assertTrue($acl->isDenied('user', 'canDelete'), 'User canDelete should be denied');
+        $this->assertNotTrue($acl->isDenied('user', 'canCreate'), 'User canCreate should be allowed');
+        $this->assertTrue($acl->isDenied('user', 'canRead'), 'User canRead should be denied');
+        $this->assertTrue($acl->isDenied('guest', 'canRead'), 'Guest canRead should be denied');
+        $this->assertNotTrue($acl->isDenied('guest', 'canDelete'), 'Guest canDelete should be allowed');
+        $this->assertNotTrue($acl->isDenied('guest', 'canUpdate'), 'Guest canUpdate should be allowed');
+    }
 }
